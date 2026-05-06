5350d8358e1431e32ae324d327745936adef9751
Aanish Nithin A <aanishnithin07@gmail.com>
2026-05-06 21:37:21 +0530

    Remove sourcemap option from build configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    sourcemap: false, 
     rollupOptions: {
       output: {
         manualChunks(id) {
